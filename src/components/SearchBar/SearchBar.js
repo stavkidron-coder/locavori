c7e71068b91c1feb9ce56f91c72bd61c54912ab6
Stav Kidron <kidronstav@gmail.com>
2020-12-12 16:22:49 -0600

    collapsable search button to search created and front-end navigation functioning

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,22 +7,23 @@ class SearchBar extends Component {
 
   render() {
     return (
-        <Form inline>
-        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-            <Label for="findInput">Find:</Label>
-            <Input type="select" name="select" id="findInput">
-                <option>Everything</option>
-                <option>All Food</option>
-                <option>All Drink</option>
-                <option>Bread</option>
-                <option>Something Else</option>
-            </Input>
-        </FormGroup>
-        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Label for="locationInput" className="mr-sm-2">Near:</Label>
-          <Input type="text" name="location" id="locationInput" />
-        </FormGroup>
-        <Button>Submit</Button>
+        <Form>
+            <FormGroup>
+                <Label for="findInput">Find:</Label>
+                <Input type="select" name="select" id="findInput">
+                    <option>Everything</option>
+                    <option>All Food</option>
+                    <option>All Drink</option>
+                    <option>Bread</option>
+                    <option>Something Else</option>
+                </Input>
+            </FormGroup>
+            
+            <FormGroup>
+                <Label for="locationInput" className="mr-sm-2">Near:</Label>
+                <Input type="text" name="location" id="locationInput"/>
+            </FormGroup>
+            <Button>Submit</Button>
       </Form>
     );
   }
