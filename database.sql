32d7078e515238e1ddc27d5cf283beb8b9090ab8
Thomas Brookshaw <tbrookshaw13@gmail.com>
2020-12-16 20:47:52 -0600

    switched login from using user name to email in front and back end

--- database.sql
+++ database.sql
@@ -7,11 +7,11 @@
 -- DATABASE NAME: locavori
 
 
--- CREATE TABLE "user" (
---     "id" SERIAL PRIMARY KEY,
---     "username" VARCHAR (80) UNIQUE NOT NULL,
---     "password" VARCHAR (1000) NOT NULL
--- );
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
+);
 
 
 
