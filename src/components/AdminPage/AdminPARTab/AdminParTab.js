54705cfa7431d55c58b31fc4f1576ff31fcb7acf
Beau Weise <beauweise@gmail.com>
2020-12-17 09:20:17 -0600

    deleted admin navbar

--- src/components/AdminPage/AdminPARTab/AdminParTab.js
+++ src/components/AdminPage/AdminPARTab/AdminParTab.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 import { withRouter } from "react-router";
-import Navbar from './PARNavBar';
+
 
 
 class AdminPARTab extends Component {
@@ -11,7 +11,7 @@ class AdminPARTab extends Component {
     return (
         <>
                 <h1>Approval Request Pending</h1>  
-                <Navbar/>         
+                         
         </>
     );
   }
