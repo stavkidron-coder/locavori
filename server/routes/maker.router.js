ab9776046723ced546dc8d00ec0aa43c97a8209f
Sam Mahler <mahler.sam@gmail.com>
2020-12-18 15:30:30 -0600

    req.query testing

--- server/routes/maker.router.js
+++ server/routes/maker.router.js
@@ -16,16 +16,17 @@ router.get('/', (req, res) => {
     });
 });
 
-// router.get('/:filters', (req, res) => {
-//     const queryText = 'SELECT * FROM "tbl_artisans";';
-//     pool.query(queryText)
-//       .then(result => {
-//           res.send(result.rows);
-//       }).catch(error => {
-//           res.sendStatus(500);
-//           console.log('error in GET makers', error);
-//       });
-//   });
+router.get('/filter', (req, res) => {
+  console.log(req.query)
+    // const queryText = 'SELECT * FROM "tbl_artisans" where "business_type" = $1;';
+    // pool.query(queryText)
+    //   .then(result => {
+    //       res.send(result.rows);
+    //   }).catch(error => {
+    //       res.sendStatus(500);
+    //       console.log('error in filter makers GET', error);
+    //   });
+  });
 
 /**
  * POST route template
--- src/redux/sagas/maker.saga.js
+++ src/redux/sagas/maker.saga.js
@@ -10,8 +10,18 @@ function* getMakers() {
     }
 }
 
+function* filterMakers(action) {
+    try {
+        const makers = yield axios.get(`/api/maker/filter/?business=${action.payload.business_type}&delivery=${action.payload.delivery}&product=${action.payload.product_type}`);
+        yield put({type: 'SET_MAKER', payload: makers.data});
+    } catch (error) {
+        console.log('maker filer GET failed', error);
+    }
+}
+
 function* makerSaga() {
     yield takeEvery('GET_MAKERS', getMakers);
+    yield takeEvery('FILTER_MAKERS', filterMakers);
 }
 
 export default makerSaga;
