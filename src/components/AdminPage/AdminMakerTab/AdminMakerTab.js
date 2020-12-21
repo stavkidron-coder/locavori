f389c65aa0632d9935d5ce2271c4c6ad2d8a7585
Beau Weise <beauweise@gmail.com>
2020-12-21 16:20:48 -0600

    getting all info from makers added cards and mapping makers

--- src/components/AdminPage/AdminMakerTab/AdminMakerTab.js
+++ src/components/AdminPage/AdminMakerTab/AdminMakerTab.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 import { withRouter } from "react-router";
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Container, Button } from 'reactstrap';
+
 
 
 class Maker extends Component {
+    componentDidMount(){
+        this.getMaker();
+    }
 
+    getMaker = () => {
+        this.props.dispatch({type: 'GET_MAKERS'})
+    }
+    ViewProfile = () =>{
+        this.props.history.push("/maker")
+    }
     render() {
         return (
-            <h1>Maker</h1>
+        <Container>       
+            <h1>Makers</h1>
+            <Card>
+                {this.props.store.maker.map((maker) => {
+                    return <><Card key = {maker.id}>
+                        <CardTitle>{maker.business_name}</CardTitle>
+                        <CardBody>{maker.story}</CardBody>
+                    </Card>
+                        <Button onClick={this.ViewProfile}>View Profile</Button>
+                        <Button>Delete Profile</Button>
+                    </>
+
+                })}
+
+            </Card>
+        </Container> 
         );
     }
 }
