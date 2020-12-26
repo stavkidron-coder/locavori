72e7470f5b263bd0f1913716f70d1a9a577bdb78
Sam Mahler <mahler.sam@gmail.com>
2020-12-26 12:29:58 -0600

    contact handleChange

--- src/redux/reducers/maker_registration.reducer.js
+++ src/redux/reducers/maker_registration.reducer.js
@@ -8,6 +8,8 @@ const registrationReducer = (state={}, action) => {
         case 'REMOVE_PRODUCT_CATEGORY':
             product_category_array = product_category_array.filter(item => item !== action.payload);
             return {...state, product_category: product_category_array};
+        case 'ADD_CONTACT':
+            return {...state, contact: {...state.contact, [action.payload.key]: action.payload.value}};
         default:
             return state;
     }
