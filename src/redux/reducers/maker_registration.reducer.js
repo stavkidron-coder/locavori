c0ff9e48dedaa5ae6278ee85182801622e4bfffd
Sam Mahler <mahler.sam@gmail.com>
2020-12-26 13:46:31 -0600

    handleChange for business tab

--- src/redux/reducers/maker_registration.reducer.js
+++ src/redux/reducers/maker_registration.reducer.js
@@ -1,4 +1,6 @@
-var product_category_array = [];
+let product_category_array = [];
+let business_type_array = [];
+let business_license_array = []
 
 const registrationReducer = (state={}, action) => {
     switch (action.type) {
@@ -8,8 +10,27 @@ const registrationReducer = (state={}, action) => {
         case 'REMOVE_PRODUCT_CATEGORY':
             product_category_array = product_category_array.filter(item => item !== action.payload);
             return {...state, product_category: product_category_array};
+
         case 'ADD_CONTACT':
             return {...state, contact: {...state.contact, [action.payload.key]: action.payload.value}};
+
+        case 'ADD_BUSINESS_TYPE':
+            business_type_array.push(action.payload);
+            return {...state, business_type: business_type_array};
+        case 'REMOVE_BUSINESS_TYPE':
+            business_type_array = business_type_array.filter(item => item !== action.payload);
+            return {...state, business_type: business_type_array};
+
+        case 'ADD_BUSINESS_LICENSE':
+            business_license_array.push(action.payload);
+            return {...state, business_license: business_license_array};
+        case 'REMOVE_BUSINESS_LICENSE':
+            business_license_array = business_license_array.filter(item => item !== action.payload);
+            return {...state, business_license: business_license_array};
+
+        case 'ADD_BUSINESS_SPECS':
+            return {...state, business_specs: {...state.business_specs, [action.payload.key]: action.payload.value}};
+
         default:
             return state;
     }
