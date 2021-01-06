a5b6af7dd166df8f7bad719f7d9c73fed0630c4f
Sam Mahler <mahler.sam@gmail.com>
2021-01-06 14:48:25 -0600

    PUT working if all fields complete

--- server/routes/maker.router.js
+++ server/routes/maker.router.js
@@ -57,69 +57,10 @@ router.get('/filter', async (req, res) => {
  * PUT route template
  */
 router.put('/', (req, res) => {
-  console.log(req.body);
 
-  // // CONTACT INFO
-  // const legal_name = req.body.contact.legal_business_name;
-  // const business_name = req.body.contact.public_business_name;
-  // const first_name = req.body.contact.first_name;
-  // const last_name = req.body.contact.last_name;
-  // const email_contact = req.body.contact.email;
-  // const phone_one = req.body.contact.phone;
-  // const phone_two = req.body.contact.alt_phone;
-  // const business_address = req.body.contact.business_address;
-  // const business_address_two = req.body.contact.business_suite_num;
-  // const business_city = req.body.contact.business_city;
-  // const business_postalcode = req.body.contact.business_zip_code;
-  // const public_address = req.body.contact.public_address;
-  // const public_address_two = req.body.contact.public_suite_num;
-  // const public_city = req.body.contact.public_city;
-  // const public_postalcode = req.body.contact.public_zip_code;
-
-  // // BUSINESS SPECIFICATIONS
-  // const website = req.body.business_specs.website;
-  // const facebook = req.body.business_specs.facebook;
-  // const instagram = req.body.business_specs.instagram;
-  // const public_email = req.body.business_specs.public_email;
-  // const license_state = req.body.business_specs.license_id_state;
-  // const license = req.body.business_license;
-  // const business_type = req.body.business_type;
-
-  // // PRODUCT DISTRIBUTION
-  // const where_sold = req.body.product_distribution;
-  // const pickup = req.body.delivery_type.pick_up;
-  // const delivery = req.body.delivery_type.delivery;
-  // const shipping = req.body.delivery_type.shipping;
-  // const product_avail = req.body.availability;
-
-  // // PRODUCT TYPES **DIFFERS FROM DB**
-  // const product_type_prepared = req.body.prepared_type;
-  // const product_type_fresh = req.body.fresh_type;
-  // const product_type_beverage = req.body.beverge_type;
-  // const product_type_cat = req.body.product_category;
-  // const product_unique = req.body.product_info.unique;
-  // const awards = req.body.product_info.awards;
-  // const specialties = req.body.product_info.specialties;
-
-  // // FEATURED PRODUCTS
-  // const product_type_one = req.body.featured_products.product_one_type;
-  // const product_img_one = req.body.featured_products.product_one_image;
-  // const product_url_one = req.body.featured_products.product_one_url;
-  // const product_type_two = req.body.featured_products.product_two_type;
-  // const product_img_two = req.body.featured_products.product_two_image;
-  // const product_url_two = req.body.featured_products.product_two_url;
-  // const product_type_three = req.body.featured_products.product_three_type;
-  // const product_img_three = req.body.featured_products.product_three_image;
-  // const product_url_three = req.body.featured_products.product_three_url;
-
-  // // STORY / ABOUT ME
-  // const owner_img = req.body.story_info.profile_image;
-  // const story = req.body.story_info.story;
-  // const give_back = req.body.story_info.give_back;
-  // const anything_else = req.body.story_info.anything_else;
-
-   // CONTACT INFO
    const makerInfo =[
+
+    //  CONTACT INFO
     req.body.contact.legal_business_name,
     req.body.contact.public_business_name,
     req.body.contact.first_name,
@@ -179,12 +120,15 @@ router.put('/', (req, res) => {
     req.body.story_info.story,
     req.body.story_info.give_back,
     req.body.story_info.anything_else,
+
+    // USER ID
+    req.user.id
    ]
   // *** SALES SHEET IS SPECIALTIES IN THE MEAN TIME ***
   const queryText = `UPDATE tbl_artisans SET 
     legal_name = $1, 
     business_name = $2, 
-    fist_name = $3, 
+    first_name = $3, 
     last_name = $4, 
     email_contact = $5, 
     phone_one = $6, 
@@ -198,7 +142,7 @@ router.put('/', (req, res) => {
     public_address_two = $14, 
     public_city = $15, 
     public_zip = $16, 
-    public_state = $17
+    public_state = $17,
     website = $18,
     facebook = $19,
     instagram = $20,
@@ -210,7 +154,7 @@ router.put('/', (req, res) => {
     product_avail = $26,
     product_unique = $27,
     awards = $28,
-    sales_sheet = $29
+    sales_sheet = $29,
     product_type_one = $30,
     product_img_one = $31,
     product_url_one = $32,
@@ -223,7 +167,8 @@ router.put('/', (req, res) => {
     owner_img = $39,
     story = $40,
     give_back = $41,
-    anything_else = $42;`;
+    anything_else = $42
+    WHERE profile_id = $43;`;
     pool.query(queryText, makerInfo)
         .then(() => {
             res.sendStatus(200);
