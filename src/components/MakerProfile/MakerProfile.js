c2eed20fe36665ee3b4658daa34464279e0a6041
Stav Kidron <kidronstav@gmail.com>
2021-01-10 00:06:07 -0600

    added some formatting to maker profile

--- src/components/MakerProfile/MakerProfile.js
+++ src/components/MakerProfile/MakerProfile.js
@@ -38,23 +38,37 @@ class MakerProfilePage extends React.Component {
            return (
         <>
         {maker.approved_maker ?
+
           <Card  key={maker.id}>
             <CardBody className="MakerProfileCard">
               <Row >
                 <Col xs="6">
                   <img className="logo" src={maker.logo} width='350' alt="" />
                 </Col>
-                <Col>
+
+                <Col xs="6">
                   <CardHeader className="MakerProfileHeader" tag="h2">{maker.business_name}</CardHeader>
               
-                  <CardText className="MakerContact">{maker.business_city},{maker.business_state}
+                  <CardText className="MakerContact">
+                    Located in: <br/>
+                    {maker.business_city},{maker.business_state}
                   </CardText>
-                  <CardText className="MakerContact">{maker.phone_one}</CardText>
-                  <CardText className="MakerContact">{maker.email_contact}</CardText>
+
+                  <CardText className="MakerContact">
+                    Business Phone: <br/>
+                    {maker.phone_one}
+                  </CardText>
+
+                  <CardText className="MakerContact">
+                    Email: <br/>
+                    {maker.email_contact}
+                  </CardText>
  
              
                   {/* facebook, instagram, and "like" icons go here */}
-                 
+
+                  <Row>
+                    <Col>
                     <Button className="WebsiteLink" href={maker.website}>visit website</Button>
 
                     <a href={maker.facebook}>
@@ -64,14 +78,15 @@ class MakerProfilePage extends React.Component {
                     <a href={maker.facebook}>
                       <p className="SocialIcon">{instagram}</p>
                     </a>
-                  
+                    </Col>
+                  </Row>
                 
                 </Col>
               </Row>
 
               <Row className="MeetTheMaker">
                 <Col xs="8">
-                  <CardSubtitle className="MakerTitle" tag="h4" >Meet {maker.first_name} from {maker.business_name}</CardSubtitle>
+                  <CardSubtitle className="MakerTitle" tag="h4">Meet {maker.first_name} from {maker.business_name}</CardSubtitle>
                   <CardText>{maker.story}</CardText>
 
                   <CardSubtitle>Known for:</CardSubtitle>
@@ -82,24 +97,25 @@ class MakerProfilePage extends React.Component {
                 </Col>
                 
                 <Col xs="4">
-                <img className="MakerImage" src={maker.owner_img} width='250' alt="" />
+                <img className="MakerImage" src={maker.owner_img} alt="maker-img" />
                 </Col>
               </Row>
 
+              <h2>Featured Products</h2>
               <Row className="FeaturedProducts">
                 <Col xs="4">
                   <a className="ProductTitle" href={maker.product_url_one}>
-                    <CardSubtitle  href={maker.product_url_one}>{maker.product_type_one}</CardSubtitle>
+                    <h4>{maker.product_type_one}</h4>
                   </a> 
 
                   <a href={maker.product_url_one}>
-                    <img  className="FeaturedProductImg" src={maker.product_img_one} width='250' alt="" align="center"/>
+                    <img className="FeaturedProductImg" src={maker.product_img_one} alt="product 1"/>
                   </a>
                 </Col>
 
                 <Col xs="4">
                   <a className="ProductTitle" href={maker.product_url_two}>
-                    <CardSubtitle >{maker.product_type_two}</CardSubtitle>
+                    <h4>{maker.product_type_two}</h4>
                   </a>
 
                   <a href={maker.product_url_two}>
@@ -109,7 +125,7 @@ class MakerProfilePage extends React.Component {
 
                 <Col xs="4">
                   <a className="ProductTitle" href={maker.product_url_three}>
-                    <CardSubtitle >{maker.product_type_three}</CardSubtitle>
+                    <h4>{maker.product_type_three}</h4>
                   </a>
 
                   <a href={maker.product_url_three}>
